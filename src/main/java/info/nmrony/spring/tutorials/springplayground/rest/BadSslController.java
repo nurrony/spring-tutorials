5ae9ba19fdda81b02c1711fd1ca1b7e0684a0976
Nur Rony <pro.nmrony@gmail.com>
2022-07-02 14:46:01 +0600

    add self-signed cert tutorial spring

--- src/main/java/info/nmrony/spring/tutorials/springplayground/rest/BadSslController.java
+++ src/main/java/info/nmrony/spring/tutorials/springplayground/rest/BadSslController.java
@@ -1,30 +1,35 @@
 package info.nmrony.spring.tutorials.springplayground.rest;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import info.nmrony.spring.tutorials.springplayground.domain.httpclient.HttpRequester;
 import lombok.Data;
 
+/**
+ * @author Nur Rony
+ */
 @RestController
 @RequestMapping("/api/ssl-tester")
 public class BadSslController {
 
-    @GetMapping("selfsigned")
-    public void get() throws Exception {
-        String selfsigned = "https://self-signed.badssl.com/";
-        // String validSsl = "https://reqbin.com/echo/get/json";
-
+    @GetMapping("self-signed")
+    public EchoResponse get() throws Exception {
+        // String url = "https://self-signed.badssl.com/";
+        String url = "https://httpbin.org/get?working=fine";
         HttpRequester request = new HttpRequester(true);
-        var json = request.getJson(selfsigned, EchoResponse.class, new HashMap<String, String>());
-        System.out.println("test " + json);
+        return request.getJson(url, EchoResponse.class, new HashMap<String, String>());
     }
 }
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 class EchoResponse {
-    String success;
+    Map<String, Object> args;
 }
