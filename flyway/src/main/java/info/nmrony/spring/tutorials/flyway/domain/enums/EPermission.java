6f84963166b660e66204b4b4f823f4108500057f
Nur Rony <pro.nmrony@gmail.com>
2022-08-27 23:59:00 +0600

    add flyway tutorial

---
+++ flyway/src/main/java/info/nmrony/spring/tutorials/flyway/domain/enums/EPermission.java
@@ -0,0 +1,8 @@
+package info.nmrony.spring.tutorials.flyway.domain.enums;
+
+public enum EPermission {
+    READ,
+    WRITE,
+    UPDATE,
+    DELETE
+}
---
+++ flyway/src/main/java/info/nmrony/spring/tutorials/flyway/domain/enums/ERole.java
@@ -0,0 +1,7 @@
+package info.nmrony.spring.tutorials.flyway.domain.enums;
+
+public enum ERole {
+    ROLE_ADMIN,
+    ROLE_MODERATOR,
+    ROLE_USER,
+}
