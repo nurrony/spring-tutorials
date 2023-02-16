12270efcf7cee7d4d018a1052f543908c5f0b43e
Nur Rony <pro.nmrony@gmail.com>
2023-02-16 13:28:03 +0600

    remove apache camel tutotiral for better one

---
+++ spring-microsoervices/products/src/test/java/info/nmrony/tutorials/spring/microservice/products/SpringMicroserviceProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nmrony.tutorials.spring.microservice.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
