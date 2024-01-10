5fe1a96a6a0ca0bc30a7a13c99907e4ac061133d
Nur Rony <pro.nmrony@gmail.com>
2024-01-10 22:19:32 +0600

    update websocket example

---
+++ override-rest/src/test/java/info/nurrony/tutorials/spring/overriderest/RestOverrideApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nurrony.tutorials.spring.overriderest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestOverrideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ websocket/src/test/java/info/nurrony/tutorials/spring/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nurrony.tutorials.spring.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
