9e3ed1c1b5cb985e687c70241c63c9255d277211
Nur Rony <pro.nmrony@gmail.com>
2024-01-10 21:58:07 +0600

    add new test file

---
+++ completablefuture-and-exception/src/test/java/info/nurrony/tutorials/spring/cf/SpringCompletableFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nurrony.tutorials.spring.cf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompletableFeatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
