88040d0b29394c3216f77f761b563f26775abde1
Nur Rony <pro.nmrony@gmail.com>
2024-01-10 21:57:36 +0600

    add batch example

---
+++ batch-processing/src/test/java/info/nmrony/tutorials/spring/batch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nmrony.tutorials.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
