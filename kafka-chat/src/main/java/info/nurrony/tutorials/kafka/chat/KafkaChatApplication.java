d00f0d74e5e501bf8812c4470d16d5b8641504da
Nur Rony <pro.nmrony@gmail.com>
2024-01-10 22:00:03 +0600

    add kafka chat example

---
+++ kafka-chat/src/main/java/info/nurrony/tutorials/kafka/chat/KafkaChatApplication.java
@@ -0,0 +1,13 @@
+package info.nurrony.tutorials.kafka.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaChatApplication.class, args);
+	}
+
+}
---
+++ kafka-chat/src/test/java/info/nurrony/tutorials/kafka/chat/KafkaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nurrony.tutorials.kafka.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
