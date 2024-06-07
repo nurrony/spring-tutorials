6b3af0da34d41ad0eef01ba04499590fcc8f7fd9
Nur Rony <pro.nmrony@gmail.com>
2024-06-07 11:42:01 +0600

    update pom

--- kafka/orders/src/main/java/info/nmrony/spring/tutorials/ms/orders/OrdersApplication.java
+++ kafka/orders/src/main/java/info/nmrony/spring/tutorials/ms/orders/OrdersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrdersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrdersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApplication.class, args);
+    }
 
 }
